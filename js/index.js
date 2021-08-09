4d77ff48a138d84dd36bd40dcf102957343fef56
pHoeniX-svg <vansomecsam@gmail.com>
2021-08-09 15:06:10 +0100

    added app skeleton and structure

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(document.querySelectorAll('*'), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  } else {
+    console.log('no overflow');
+  }
+});
