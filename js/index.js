44d52d9626c32c39cf67ce84b2724684be8a437c
Prince Muel <vansomecsam@gmail.com>
2024-07-07 23:27:08 +0100

    chore: remove unnecessary files
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- js/index.js
+++
@@ -1,6 +0,0 @@
-//This function inspects overflowing elements 
-const docWidth = document.documentElement.offsetWidth;
-[].forEach.call(document.querySelectorAll('*'), (el) =>
-  console.log(el.offsetWidth > docWidth)
-);
-/*////*/
--- js/range.js
+++
@@ -1,7 +0,0 @@
-const _R = document.querySelector('[type=range]');
-
-document.documentElement.classList.add('js');
-
-_R.addEventListener('input', e => {
-  _R.style.setProperty('--val', +_R.value)
-}, false);
