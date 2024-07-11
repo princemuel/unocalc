232135885bde2616f98fcc77f83a577b14477f92
Prince Muel <vansomecsam@gmail.com>
2024-07-11 15:56:43 +0100

    feat: add math operations

--- src/utils/operations.rs
+++ src/utils/operations.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Operation {
     Add,
     Subtract,
