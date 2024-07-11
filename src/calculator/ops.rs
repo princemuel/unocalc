a1cf6a77990a96ff6b09c13bb84fc1f377e1ab8e
Prince Muel <vansomecsam@gmail.com>
2024-07-11 14:58:56 +0100

    feat: add math operations

---
+++ src/calculator/ops.rs
@@ -0,0 +1,23 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+#[derive(Debug, Clone, PartialEq)]
+pub enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+
+impl Operation {
+    pub fn from_char(value: char) -> Option<Self> {
+        use Operation::*;
+        match value {
+            '+' => Some(Add),
+            '-' => Some(Subtract),
+            '*' => Some(Multiply),
+            '/' => Some(Divide),
+            _ => None,
+        }
+    }
+}
