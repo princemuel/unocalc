d495915061f672176d71e5c9079abd7fc8df11f6
Prince Muel <vansomecsam@gmail.com>
2024-07-11 12:47:43 +0100

    feat: add number types implicit coversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,30 @@
 mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Number {
+    Int(i64),
+    Float(f64),
+}
+
+impl From<i64> for Number {
+    fn from(value: i64) -> Self {
+        Number::Int(value)
+    }
+}
+
+impl From<f64> for Number {
+    fn from(value: f64) -> Self {
+        Number::Float(value)
+    }
+}
+
+impl Number {
+    fn to_float(&self) -> f64 {
+        match self {
+            Number::Int(value) => *value as f64,
+            Number::Float(value) => *value,
+        }
+    }
+}
