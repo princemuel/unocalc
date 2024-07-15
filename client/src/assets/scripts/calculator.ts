cec6b019ad56cf4ac00ca3b2dfdbe06b1fdfc5cf
Prince Muel <vansomecsam@gmail.com>
2024-07-15 20:02:02 +0100

    feat: update calculator client code

--- client/src/assets/scripts/calculator.ts
+++ client/src/assets/scripts/calculator.ts
@@ -2,7 +2,7 @@ import { Calculator, Operation } from "unocalc";
 import { getElement } from "./dom";
 import { number_guard } from "./utils";
 
-const calculator = Calculator.new();
+const calculator = new Calculator();
 
 const operations = new Map([
   ["+", Operation.Add],
@@ -20,9 +20,9 @@ form.addEventListener("click", (e) => {
   if (key instanceof HTMLButtonElement) {
     const key_type = key.dataset.type;
 
-    if (key_type === "number") {
+    if (key_type === "number" || key_type === "decimal") {
       const operator = (key.textContent ?? "").trim().toLowerCase();
-      calculator.input_digit(operator);
+      calculator.set_digit(operator);
     }
 
     if (key_type === "operator") {
@@ -31,14 +31,11 @@ form.addEventListener("click", (e) => {
       if (operation) calculator.set_operation(operation);
     }
 
-    if (key_type === "decimal") calculator.input_digit(".");
     if (key_type === "delete") calculator.backspace();
     if (key_type === "reset") calculator.reset();
     if (key_type === "equals") calculator.calculate();
 
-    const result = number_guard(
-      calculator.input_buffer() || calculator.current_value()
-    );
+    const result = number_guard(calculator.get_current_value());
     output.textContent = new Intl.NumberFormat().format(result);
   }
 });
