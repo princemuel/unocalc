102f51b630c67685de139a92c6656860bd079154
Prince Muel <vansomecsam@gmail.com>
2024-07-17 03:41:10 +0100

    feat: update pwa web mainfest
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,13 @@ import top_level_await from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [wasm(), top_level_await(), pwa({ registerType: "autoUpdate" })],
+  plugins: [
+    wasm(),
+    top_level_await(),
+    pwa({
+      registerType: "autoUpdate",
+      pwaAssets: { disabled: false, config: true },
+    }),
+  ],
   server: { port: 3000 },
 });
