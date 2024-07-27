f55c5da2df28f35a24b3b4d881dbd02ee55c97cb
Prince Muel <vansomecsam@gmail.com>
2024-07-27 06:37:22 +0100

    test: scaffold parse token test

--- tests/web.rs
+++ tests/web.rs
@@ -3,17 +3,40 @@
 // #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use unocalc::parse_to_tokens;
+use unocalc::schema::Token;
 use wasm_bindgen_test::*;
 
 extern crate unocalc;
 
-wasm_bindgen_test_configure!()
-
+wasm_bindgen_test_configure!();
 
 #[wasm_bindgen_test]
-fn tester(){
-    test_fn();
+fn test_parse_tokens() {
+    use Token::*;
+
+    assert_eq!(
+        parse_to_tokens(test_expression_b()),
+        Ok(vec![
+            Number(-5.0),
+            Operator('+'),
+            Number(3.14),
+            Operator('*'),
+            Paren(true),
+            Number(2.0),
+            Operator('-'),
+            Number(1.0),
+            Paren(false)
+        ])
+    );
 }
 
 #[cfg(test)]
-pub fn test_fn()  {}
+pub fn test_expression_a() -> String {
+    format!("6+(4.5*(8+3/1.5)−2)/4")
+}
+
+#[cfg(test)]
+pub fn test_expression_b() -> String {
+    format!("-5 + 3.14 * ( 2 - 1 )")
+}
