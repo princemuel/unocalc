446a4184aacdaf1690524bfba5b3741d91c0e5ad
Prince Muel <vansomecsam@gmail.com>
2024-07-31 00:04:47 +0100

    chore: update repo
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,8 @@
 extern crate cfg_if;
+extern crate nom;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
-pub mod expr_node;
 pub mod lexer;
 pub mod parser;
-pub mod schema;
 pub mod utils;
-
-use wasm_bindgen::prelude::*;
-
-// A macro to provide `println!(..)`-style syntax for `console.log` logging.
-macro_rules! console_log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
-
-#[wasm_bindgen]
-
-pub fn evaluate_expression(input: String) -> String {
-    utils::set_panic_hook();
-
-    console_log!("{}", input);
-
-    input
-}
