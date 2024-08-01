b6e3ecc7c3bbde4f00b5764e9c86dbcd2be297a7
Prince Muel <vansomecsam@gmail.com>
2024-08-01 18:07:10 +0100

    build: compile calculator wasm backend
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- pkg/index_bg.js
+++ pkg/index_bg.js
@@ -53,19 +53,6 @@ function addHeapObject(obj) {
     return idx;
 }
 
-function _assertChar(c) {
-    if (typeof(c) === 'number' && (c >= 0x110000 || (c >= 0xD800 && c < 0xE000))) throw new Error(`expected a valid Unicode scalar value, found ${c}`);
-}
-
-let cachedInt32Memory0 = null;
-
-function getInt32Memory0() {
-    if (cachedInt32Memory0 === null || cachedInt32Memory0.byteLength === 0) {
-        cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
-    }
-    return cachedInt32Memory0;
-}
-
 let WASM_VECTOR_LEN = 0;
 
 const lTextEncoder = typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder;
@@ -124,101 +111,14 @@ function passStringToWasm0(arg, malloc, realloc) {
     return ptr;
 }
 /**
+* @param {string} value
+* @returns {number}
 */
-export const Operation = Object.freeze({ Add:1,"1":"Add",Subtract:2,"2":"Subtract",Multiply:3,"3":"Multiply",Divide:4,"4":"Divide", });
-
-const CalculatorFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_calculator_free(ptr >>> 0));
-/**
-*/
-export class Calculator {
-
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        CalculatorFinalization.unregister(this);
-        return ptr;
-    }
-
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_calculator_free(ptr);
-    }
-    /**
-    */
-    constructor() {
-        const ret = wasm.calculator_new();
-        this.__wbg_ptr = ret >>> 0;
-        return this;
-    }
-    /**
-    */
-    calculate() {
-        wasm.calculator_calculate(this.__wbg_ptr);
-    }
-    /**
-    * @param {string} value
-    */
-    set_digit(value) {
-        const char0 = value.codePointAt(0);
-        _assertChar(char0);
-        wasm.calculator_set_digit(this.__wbg_ptr, char0);
-    }
-    /**
-    * @param {Operation} operation
-    */
-    set_operation(operation) {
-        wasm.calculator_set_operation(this.__wbg_ptr, operation);
-    }
-    /**
-    */
-    backspace() {
-        wasm.calculator_backspace(this.__wbg_ptr);
-    }
-    /**
-    */
-    reset() {
-        wasm.calculator_reset(this.__wbg_ptr);
-    }
-    /**
-    * @returns {string}
-    */
-    get_current_value() {
-        let deferred1_0;
-        let deferred1_1;
-        try {
-            const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-            wasm.calculator_get_current_value(retptr, this.__wbg_ptr);
-            var r0 = getInt32Memory0()[retptr / 4 + 0];
-            var r1 = getInt32Memory0()[retptr / 4 + 1];
-            deferred1_0 = r0;
-            deferred1_1 = r1;
-            return getStringFromWasm0(r0, r1);
-        } finally {
-            wasm.__wbindgen_add_to_stack_pointer(16);
-            wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
-        }
-    }
-    /**
-    * @returns {string}
-    */
-    get_input_buffer() {
-        let deferred1_0;
-        let deferred1_1;
-        try {
-            const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-            wasm.calculator_get_input_buffer(retptr, this.__wbg_ptr);
-            var r0 = getInt32Memory0()[retptr / 4 + 0];
-            var r1 = getInt32Memory0()[retptr / 4 + 1];
-            deferred1_0 = r0;
-            deferred1_1 = r1;
-            return getStringFromWasm0(r0, r1);
-        } finally {
-            wasm.__wbindgen_add_to_stack_pointer(16);
-            wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
-        }
-    }
+export function calculate(value) {
+    const ptr0 = passStringToWasm0(value, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.calculate(ptr0, len0);
+    return ret;
 }
 
 export function __wbindgen_object_drop_ref(arg0) {
@@ -228,31 +128,6 @@ export function __wbindgen_object_drop_ref(arg0) {
 export function __wbindgen_string_new(arg0, arg1) {
     const ret = getStringFromWasm0(arg0, arg1);
     return addHeapObject(ret);
-};
-
-export function __wbg_new_abda76e883ba8a5f() {
-    const ret = new Error();
-    return addHeapObject(ret);
-};
-
-export function __wbg_stack_658279fe44541cf6(arg0, arg1) {
-    const ret = getObject(arg1).stack;
-    const ptr1 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    const len1 = WASM_VECTOR_LEN;
-    getInt32Memory0()[arg0 / 4 + 1] = len1;
-    getInt32Memory0()[arg0 / 4 + 0] = ptr1;
-};
-
-export function __wbg_error_f851667af71bcfc6(arg0, arg1) {
-    let deferred0_0;
-    let deferred0_1;
-    try {
-        deferred0_0 = arg0;
-        deferred0_1 = arg1;
-        console.error(getStringFromWasm0(arg0, arg1));
-    } finally {
-        wasm.__wbindgen_free(deferred0_0, deferred0_1, 1);
-    }
 };
 
 export function __wbg_log_5bb5f88f245d7762(arg0) {
