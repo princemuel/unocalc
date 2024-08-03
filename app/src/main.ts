a5029ef1f9b194ad87bf985320fe6a0de238a283
Prince Muel <vansomecsam@gmail.com>
2024-08-03 01:23:42 +0100

    feat: scaffold PWA assets gen and service worker
    Reference-To: #15
    Reviewed-by: Prince Muel <vansomecsam@gmail.com>
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,7 @@
 // asynchronously. This `main.js` file does the single async import, so
 // that no one else needs to worry about it again.
 import { inject } from "@vercel/analytics";
+import "./scripts/pwa.ts";
 import "./styles/global.css";
 import("./scripts/index.ts").catch((e) =>
   console.error("Error importing `index.ts`:", e)
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,16 +1,72 @@
 import { defineConfig } from "vite";
-import { VitePWA as pwa } from "vite-plugin-pwa";
+import { VitePWA as pwa, VitePWAOptions } from "vite-plugin-pwa";
 import top_level_await from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
+// import manifest from "./manifest.json"
+
+const manifest = (function () {
+  type ManifestPromise = Promise<VitePWAOptions["manifest"]>;
+  try {
+    return import("./manifest.json").then((r) => r.default) as ManifestPromise;
+  } catch (error) {
+    return {} as ManifestPromise;
+  }
+})();
 
 export default defineConfig({
+  server: { port: 3000 },
   plugins: [
     wasm(),
     top_level_await(),
     pwa({
       registerType: "autoUpdate",
-      pwaAssets: { disabled: false, config: true },
+      pwaAssets: { disabled: false, config: true, overrideManifestIcons: true },
+      manifest: await manifest,
+      devOptions: { enabled: true, suppressWarnings: true, type: "module" },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "google-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "gstatic-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === "image",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "images-cache",
+              expiration: {
+                maxEntries: 10,
+              },
+            },
+          },
+        ],
+      },
     }),
   ],
-  server: { port: 3000 },
 });
